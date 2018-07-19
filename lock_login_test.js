f26bb3dde132f1e85bff2a8f0fc5ca9bbbeaaaa0
Alexis Luque <aluque@indexlabs.com.uy>
2018-07-19 18:00:31 -0300

    Update lock_login_test.js

--- lock_login_test.js
+++ lock_login_test.js
@@ -2,6 +2,8 @@ Feature('SPA login');
 
 Scenario('Log in using Lock', (I) => {
     I.amOnPage('/');
+    I.waitForElement('button.btn.btn-default.navbar-btn', 3);
+    I.click('Accept');
     I.waitForVisible('#qsLoginBtn', 3);
     I.click('#qsLoginBtn');
     I.waitForVisible('input[name="password"]', 5);
@@ -9,5 +11,6 @@ Scenario('Log in using Lock', (I) => {
     I.fillField('input[name="username"]', 'asdasd');
     I.fillField('input[name="password"]', 'asdasd');
     I.click('.auth0-lock-widget-container .auth0-lock-submit');
+    I.wait(1);
     I.retry(2).waitForVisible('#qsLogoutBtn', 5);
 });
